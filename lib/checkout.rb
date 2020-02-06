95becafd413e4ba4648c925f98d69989670f5bb2
M.Bellucci <delbetu@gmail.com>
2020-02-06 19:26:05 -0300

    Implement default rule using the Invoice structure.

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,17 +1,17 @@
 Item = Struct.new(:code, :name, :price)
 
-InvoiceItem = Struct.new(:product_code, :count, :price_per_unit, :charged_unit_price, :charged_amount)
+InvoiceItem = Struct.new(:product_code, :count, :price_per_unit, :charged_unit_price, :charged_count)
 
 class Invoice
   def initialize(basket)
     # [ ('FR1', 55 items, 20 $ each) ]
     @invoice_items = basket.group_by { |item| item.code }
-      .map { |code, items| InvoiceItem.new(code, items.length, items.first.price) }
+      .map { |code, items| InvoiceItem.new(code, items.length, items.first.price, items.first.price, items.length) }
   end
 
   def total
     @invoice_items.inject(0) do |total, invoice_item|
-      total + ( invoice_item.charged_unit_price * invoice_item.charged_amount )
+      total + ( invoice_item.charged_unit_price * invoice_item.charged_count )
     end
   end
 end
@@ -30,7 +30,8 @@ class Checkout
     if @pricing_rules.include?('buy-one-get-one-free')
       BuyOneGetOneRule.new(@basket).total
     else
-      DefaultRule.new(@basket).total
+      invoice = DefaultRule.new(@basket).apply
+      invoice.total
     end
   end
 
@@ -55,9 +56,6 @@ class Rule
 end
 
 class DefaultRule < Rule
-  def total
-    @basket.inject(0) { |total, purchased_product| total + purchased_product.price }
-  end
 end
 
 class BuyOneGetOneRule < Rule
