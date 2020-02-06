c1e6996e4444fb348ba1a2b46ee46f2f8b8e9384
M.Bellucci <delbetu@gmail.com>
2020-02-06 17:11:25 -0300

    Adding twice the same item.

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,10 +1,20 @@
 require 'checkout'
 
 describe Checkout do
-  it 'accumulates items and returns the total' do
-    item = Item.new('FR1', 'product1', 25.5)
-    subject = Checkout.new
-    subject.scan(item)
-    expect(subject.total).to eq 25.5
+  describe 'accumulating item prices' do
+    it 'returns the only item price added to the basket' do
+      item = Item.new('FR1', 'product1', 25.5)
+      subject = Checkout.new
+      subject.scan(item)
+      expect(subject.total).to eq 25.5
+    end
+
+    it 'returns 2 times the total of the item when adding same item twice' do
+      item = Item.new('FR1', 'product1', 25.5)
+      subject = Checkout.new
+      subject.scan(item)
+      subject.scan(item)
+      expect(subject.total).to eq 51.0
+    end
   end
 end
